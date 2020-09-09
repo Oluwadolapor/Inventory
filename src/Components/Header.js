03cbabc3104b7e57a54ba090fb102c0baff41a16
Onifade Dolapo <hardolipey@gmail.com>
2020-09-09 13:03:31 +0100

    Header component created

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Header extends Component{
+    render(){
+        return(
+            <div className="header">
+                <h1>To-Do List</h1>
+            </div>
+        )
+    }
+}
+export default Header
