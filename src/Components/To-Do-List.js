83a1b9b727f06a44a0e241f5efa4c8b799f4148d
Onifade Dolapo <hardolipey@gmail.com>
2020-09-09 13:17:34 +0100

    To-Do-List component created

---
+++ src/Components/To-Do-List.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class List extends Component{
+    render(){
+        return(
+            <div className='list-container'>
+                <ul className="list"></ul>
+            </div>
+        )
+    }
+}
+
+export default List
