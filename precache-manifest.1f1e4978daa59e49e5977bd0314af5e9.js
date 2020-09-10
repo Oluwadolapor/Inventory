8fe5a5e15b3a343a685252f8ffad5625afd6664b
Onifade Dolapo <hardolipey@gmail.com>
2020-09-10 15:53:14 +0100

    Updates

---
+++ precache-manifest.1f1e4978daa59e49e5977bd0314af5e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168dbd9654002b0d042d2afe35b4b142",
+    "url": "/Inventory/index.html"
+  },
+  {
+    "revision": "9cbff57c7c0fdf5a8dfd",
+    "url": "/Inventory/static/css/main.557207a6.chunk.css"
+  },
+  {
+    "revision": "cab8573a58a3f7484f59",
+    "url": "/Inventory/static/js/2.5709d804.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Inventory/static/js/2.5709d804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cbff57c7c0fdf5a8dfd",
+    "url": "/Inventory/static/js/main.8f5c7a0f.chunk.js"
+  },
+  {
+    "revision": "14cb5ab98784251022d0",
+    "url": "/Inventory/static/js/runtime-main.925746c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Inventory/static/media/logo.5d5d9eef.svg"
+  }
+]);
