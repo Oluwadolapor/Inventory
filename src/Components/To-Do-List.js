662bd6acb8d6652b265cc4b3c9150585ba3a8cf5
Onifade Dolapo <hardolipey@gmail.com>
2020-09-10 15:56:04 +0100

    Work Done

--- src/Components/To-Do-List.js
+++ src/Components/To-Do-List.js
@@ -9,7 +9,7 @@ class List extends Component{
                 <ul className="listClass">
                     {this.props.lists.map((list, index) => {
                         return <ol className="list swing" key={index}>{list}
-                        {/* <--<button onClick={(e) => this.props.deleteList(e,index)}><i className="fas fa-trash"></i></button>--> */}
+                        <button onClick={(e) => this.props.deleteList(e,index)}><i className="fas fa-trash"></i></button>
                         </ol>
 
                     })}
